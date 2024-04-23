637bce11520f42a313f893b4504554ed1e8f9416
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2024-04-23 16:36:57 -0400

    have wordpress

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
 
-if [ ! -d "/var/lib/mysql/$DB_NAME" ]
-then
+#if [ ! -d "/var/lib/mysql/$DB_NAME" ]
+#then
+
+#service mariadb start
+
+#echo -e "\nY\nY\n$DB_PASSWORD\n1234\nY\nY\nY\nY\n" | mysql_secure_installation
+
+#mysql << EOF
+
+#CREATE DATABASE $DB_NAME;
+#CREATE USER $DB_USER@'%' IDENTIFIED BY '$DB_PASSWORD';
+#GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%' IDENTIFIED BY '$DB_PASSWORD';
+#FLUSH PRIVILEGES;
+#exit;
+#EOF
+
+#service mariadb stop
+
+#fi
+
+#@ -> entrypoint
+#exec "$@"
 
 service mariadb start
 
-echo -e "\nY\nY\n$DB_\n1234\nY\nY\nY\nY\n" | mysql_secure_installation
+if [ ! -d "/var/lib/mysql/$DB_NAME" ]
+then
+	# Secure Databases
+	echo -e "\nY\nY\n$DB_ROOT\n$DB_ROOT\nY\nY\nY\nY" | mysql_secure_installation
 
-mysql << EOF
-
-CREATE DATABASE $DB_NAME;
-CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-FLUSH PRIVILEGES;
-exit;
-EOF
+	# Init Databases
+	mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+	mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+	mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+	mariadb -e "FLUSH PRIVILEGES;"
+fi
 
 service mariadb stop
 
-fi
-
-#@ -> entrypoint
 exec "$@"
+
+
